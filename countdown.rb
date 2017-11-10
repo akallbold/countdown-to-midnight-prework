eee22df1f9bfd5dbe828c269f4e11b434b1529cc
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-10 19:04:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,16 @@ def countdown(n)
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+
+def countdown_with_sleep(n)
+  curr_n=n
+
+  while curr_n>0
+    puts "#{curr_n} SECOND(S)!"
+    curr_n-=1
+  end
+  return "HAPPY NEW YEAR!"
+
+end
