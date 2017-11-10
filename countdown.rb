767db572a1753ef92af8ba7cdd7d7cbb09a6f6c7
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-10 19:01:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  curr_n=n
+
+  while n>0
+    if curr_n==1
+    puts "1 SECOND!"
+    else
+      puts "#{curr_n} SECONDS!"
+    end
+    
+    curr_n-=1
+    
+  end
 end
